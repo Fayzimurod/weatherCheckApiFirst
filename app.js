c4a0545d2e6dee7066bdf996374e8ed6664be475
Fayzimurod <inogamovsayfutdin051@gmail.com>
2025-05-08 12:16:49 +0500

    weather-api-small

---
+++ app.js
@@ -0,0 +1,27 @@
+// ; const apiKey = "8709f88f1a6cc4c7acf0d457106eb048"
+// const apiKey = "8709f88f1a6cc4c7acf0d457106eb048"; // OpenWeatherMap API kalitingizni bu yerga joylashtiring
+
+const apiKey = "d5a3a047d0ce2a989e9d8706938e3e52";
+   document.getElementById("searchBtn").addEventListener("click", () => {
+      const city = document.getElementById("cityInput").value;
+      if (city === "") return;
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      )
+         .then((response) => response.json())
+         .then((data) => {
+            document.getElementById(
+               "temperature"
+            ).innerText = `${Math.round(data.main.temp)}°C`;
+            document.getElementById("description").innerText =
+               data.weather[0].description;
+            document.getElementById(
+               "weatherIcon"
+            ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+         })
+         .catch((error) => {
+            console.error("Xatolik yuz berdi:", error);
+            alert("Shahar topilmadi!");
+         });
+   });
